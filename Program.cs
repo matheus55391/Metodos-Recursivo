7f7c8f5fe1701509f0656e8bb351dacdb62cfe1b
matheu55391 <matheus.manulo55@gmail.com>
2021-03-29 16:31:23 -0300

    v1.0

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Matheus Felipe Vieira Santiago
+
+namespace ProjetoAvonale
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+
+            int[] array = { 13, 23, 55, 10, 12 };
+
+            double media = Recursivo.Media(array, array.Length - 1);
+            Console.Write("Media: "+media + "\n");
+
+
+            List<object> lista = new List<object>() { 13, 23, 55};
+
+            List<object> lista2 = Recursivo.InverterLista(lista, new List<object>(), lista.Count - 1);
+            string resposta = "";
+            foreach (object l in lista2)
+            {
+                resposta = resposta + "/" + l;
+            }
+            Console.WriteLine(resposta);
+        }
+
+    }
+
+
+}
+
---
+++ Recursivo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoAvonale
+{
+    class Recursivo
+    {
+        public static double Media(int[] nums, int tamanho)
+        {
+            //Recursivo que imprima a média dos elementos de uma lista de inteiros e o número de elementos maiores do que a média.
+
+            if (tamanho > 0)
+            {
+                return (double)((double)nums[tamanho] + ((tamanho + 1) - 1) * Media(nums, tamanho - 1)) / (tamanho + 1);
+            }
+            else
+            {
+                return (double)nums[0];
+            }
+
+        }
+
+
+        public static List<object> InverterLista(List<object> lista, List<object> listaGenerica, int tamanho)
+        {
+            //Recursivo que retorne uma lista de forma invertida.
+            if (tamanho < 0)
+            {
+                lista = listaGenerica;
+
+                return lista;
+            }
+            else
+            {
+                listaGenerica.Add(lista[tamanho]);
+                tamanho--;
+                return InverterLista(lista, listaGenerica, tamanho);
+            }
+        }   
+    }
+}
